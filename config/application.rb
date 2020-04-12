3773ac0e85d326162d0254bbf1cf35db85a89a8c
yuki_m <yakan.meroyan@gmail.com>
2020-04-12 16:50:04 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module App015Expensemanagement
     end
 
     config.i18n.default_locale = :ja
-    config.i18n.default_locale = :jack_o_lantern
+    # config.i18n.default_locale = :jack_o_lantern
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
