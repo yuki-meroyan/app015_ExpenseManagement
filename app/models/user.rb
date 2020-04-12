abc8bb9cbfbdb8f11a1b21fddf87df7e1135076d
yuki_m <yakan.meroyan@gmail.com>
2020-04-12 20:27:52 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many   :groups      , through: group_users
+  has_many   :groups      , through: :group_users
   has_many   :group_users
   belongs_to :expense
 
