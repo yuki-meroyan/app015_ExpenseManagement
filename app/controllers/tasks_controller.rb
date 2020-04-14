1716d6c8ab04219cd23d073fc7ca7431ac107970
yuki_m <yakan.meroyan@gmail.com>
2020-04-14 22:22:31 +0900

    Update tasks_controller.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,26 @@
 class TasksController < ApplicationController
   def index
   end
+
+  def new
+    @task = Tasks.new
+  end
+
+  def create
+    @task = Beginner.new(beginner_params)
+    if @task.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  def show
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :client_user, :client_name, :detail, :request_date, :delivery_date, :group_id).merge(user_id: current_user.id)
+  end
+
 end
