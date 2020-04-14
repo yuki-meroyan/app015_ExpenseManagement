3251c6fe05caf5c08c057d6c13319c8d9b2eaedc
yuki_m <yakan.meroyan@gmail.com>
2020-04-14 23:52:51 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_125803) do
+ActiveRecord::Schema.define(version: 2020_04_14_145112) do
 
   create_table "expenses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.date "order_date", null: false
@@ -73,6 +73,9 @@ ActiveRecord::Schema.define(version: 2020_04_14_125803) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "family_name"
+    t.string "first_name"
+    t.string "nick_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
