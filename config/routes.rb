79be0d09f8e61bc11c676e38d3569dab4fd7d4ec
yuki_m <yakan.meroyan@gmail.com>
2020-04-14 22:23:42 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
   root to: "home#index"
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :groups do
-    resources :expenses
-    resources :tasks
-  end
+  resources :groups
+  resources :expenses
+  resources :tasks
 
 end
