dd86f65a9850ae32bfc585d8529a7a37f6c920ed
yuki_m <yakan.meroyan@gmail.com>
2020-04-18 23:54:22 +0900

    Update form.js

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -11,3 +11,13 @@ $(function() {
     });
   });
 });
+
+$('input').on('focusin', function() {
+  $(this).parent().find('label').addClass('active');
+});
+
+$('input').on('focusout', function() {
+  if (!this.value) {
+    $(this).parent().find('label').removeClass('active');
+  }
+});
