2369c4f0e6d210f32ed0f42069577e13a3d41ac2
yuki_m <yakan.meroyan@gmail.com>
2020-04-19 23:59:02 +0900

    Adjustment_View

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,23 +1,12 @@
 $(function() {
-  $(document).ready(function(){
-    $('.form__post__textbox').blur(function() {
-      if($(this).val().length === 0){
-        $('.form__post__label').removeClass("focus");
-      }
-      else { returns; }
-    })
-    .focus(function() {
-      $('.form__post__label').addClass("focus")
-    });
+  $('input').on('focusin', function() {
+    $(this).parent().find('label').addClass('active');
+  });
+
+  $('input').on('focusout', function() {
+    if (!this.value) {
+      $(this).parent().find('label').removeClass('active');
+    }
   });
 });
 
-$('input').on('focusin', function() {
-  $(this).parent().find('label').addClass('active');
-});
-
-$('input').on('focusout', function() {
-  if (!this.value) {
-    $(this).parent().find('label').removeClass('active');
-  }
-});
