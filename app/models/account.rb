5659c88cb7a0b91fcc3c1b4cfc7340594f88a562
yuki_m <yakan.meroyan@gmail.com>
2020-04-21 00:49:17 +0900

    Update account.rb

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,6 @@
 class Account < ActiveHash::Base
   self.data = [
-      {no: 1, name: '経費'}, {no: 2, name: '人件費'}, {no: 3, name: '雑費'},
-      {no: 4, name: '売上'}, {no: 5, name: '秋田県'}, {no: 6, name: '山形県'}
+      {no: 1, name: '経費'}, {no: 2, name: '雑費'}, {no: 3, name: '人件費'}
+    # , {no: 4, name: ''}, {no: 5, name: ''}, {no: 6, name: ''}
   ]
 end
