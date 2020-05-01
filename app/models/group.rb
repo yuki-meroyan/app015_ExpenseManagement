896b05ecc246e1ceaf146935ec94985e19e0797f
yuki_m <yakan.meroyan@gmail.com>
2020-05-01 22:55:53 +0900

    Update group.rb

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ApplicationRecord
   mount_uploader  :icon       , GroupIconsUploader
   has_one         :group_icon
   has_many        :group_users, dependent: :destroy
-  has_many        :users      , through: :group_users
-  has_many        :expenses
-  has_many        :tasks
+  has_many        :users      , through:   :group_users
+  has_many        :expenses   , dependent: :destroy
+  has_many        :tasks      , dependent: :destroy
 end
