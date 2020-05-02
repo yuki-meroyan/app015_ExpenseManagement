0d8a273019b880c044e5fb664d7ebebc1411f6e2
yuki_m <yakan.meroyan@gmail.com>
2020-05-02 22:50:10 +0900

    Update tasks_controller.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -21,10 +21,17 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-    if @task.save
-      redirect_to group_tasks_path(@group)
+    check_text = check_params(@task)
+    if check_text.blank?
+      if @task.save
+        redirect_to group_tasks_path(@group)
+      else
+        render :new, notice: 'タスクの作成に失敗しました'
+      end
     else
-      render :index
+      # redirect_to new_group_task_path, notice: check_text
+      flash.now[:alert] = check_text
+      render :new
     end
   end
 
@@ -51,6 +58,17 @@ class TasksController < ApplicationController
 
   def set_task
     @task = Task.find(params[:id])
+  end
+
+  def check_params(task)
+    text = nil
+    task.title.blank? ? text = "#{text}[タイトル]" : ""
+    task.client_user.blank? ? text = "#{text}[クライアント担当者名]" : ""
+    task.client_name.blank? ? text = "#{text}[クライアント企業名]" : ""
+    task.request_date.blank? ? text = "#{text}[依頼日]" : ""
+    task.delivery_date.blank? ? text = "#{text}[納期]" : ""
+    text != nil ? text = "#{text}を入力してください" : ""
+    return text
   end
 
 end
